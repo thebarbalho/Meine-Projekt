99c3dc5a25dc695278af35f697aac080e4270a11
thebarbalho <victorbarbalhomartins@gmail.com>
2026-04-28 15:25:36 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,16 +2,24 @@ import streamlit as st
 
 st.title('FLAMENGO - CAMPEÕES')
 st.image("https://mundodosprodutos.com.br/wp-content/uploads/2024/07/curiosidades-sobre-o-flamengo.png")
+
 st.write("1981: Raul, Leandro, Marinho, Mozer, Junior, Adílio, Andrade, Zico, Tita, Nunes, Lico, Carpegiani")         
+st.image("https://lncimg.lance.com.br/cdn-cgi/image/width=950,quality=75,fit=pad,format=webp/uploads/2025/06/lamengo-venceu-a-Libertadores-e-o-Mundial-em-1981-aspect-ratio-512-320.jpg")
 
 st.write("2013: Felipe, Léo Moura, Samir, Chicão, André Santos, Amaral, Luiz Antônio, Elias, Carlos Eduardo, Hernane, Paulinho, Jayme de Almeida")
+st.image("https://s2.glbimg.com/9CugZVkJHHbgK71ZFHZ1TqJTjB4=/620x349/top/s.glbimg.com/es/ge/f/original/2013/11/27/hernane_flamengo_final_andredurao-4_95.jpg")
 
 st.write("2019: Diego Alves, Rafinha, Rodrigo Caio, Pablo Marí, Filipe Luís, Willian Arão, Gerson, Giorgian de Arrascaeta, Éverton Ribeiro, Bruno Henrique, Gabriel B., Jorge Jesus")
+st.image("https://colunadofla.com/wp-content/uploads/2024/10/flamengo-2019.jpg")
 
 st.write("2020: Diego Alves, Isla, Rodrigo Caio, Willian Arão, Filipe Luís, Diego Ribas, Gerson, Giorgian de Arrascaeta, Éverton Ribeiro, Bruno Henrique, Gabriel B., Rogério Ceni")
+st.image("https://colunadofla.com/wp-content/uploads/2021/02/rogerio-ceni-gabigol-campeonato-brasileiro.jpeg")
 
 st.write("2022: Santos, Rodinei, David Luiz, Léo Pereira, Filipe Luís, João Gomes, Thiago Maia, Giorgian de Arrascaeta, Éverton Ribeiro, Gabriel B., Pedro, Dorival Jr.")
+st.image("https://colunadofla.com/wp-content/uploads/2022/09/poster-flamengo-x-velez-libertadores.jpg")
 
 st.write("2024: Rossi, Wesley, Léo Ortiz, Léo Pereira, Alex Sandro, Pulgar, Gerson, Giorgian de Arrascaeta, Gonzalo Plata, Gabriel B., Bruno Henrique, Filipe Luís")
+st.image("https://lncimg.lance.com.br/cdn-cgi/image/width=950,quality=75,fit=pad,format=webp/wp-content-galerias/uploads/2024/11/flamengo-campeao-scaled.jpg")
 
 st.write("2025: Rossi, Varela, Danilo, Léo Pereira, Alex Sandro, Pulgar, Jorginho, Giorgian de Arrascaeta, Samuel Lino, Bruno Henrique, Pedro, Filipe Luís")
+st.image("https://a3.espncdn.com/combiner/i?img=%2Fphoto%2F2025%2F1129%2Fr1582500_1296x729_16%2D9.jpg")
