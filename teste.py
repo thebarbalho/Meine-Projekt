28d01041a8e6d5318f23e9ebdb1101ab7a68fa28
thebarbalho <victorbarbalhomartins@gmail.com>
2026-04-28 15:15:47 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,7 +1,10 @@
 import streamlit as st
 
 st.title('FLAMENGO - CAMPEÕES')
+
 st.write("1981: Raul, Leandro, Marinho, Mozer, Junior, Adílio, Andrade, Zico, Tita, Nunes, Lico, Carpegiani")
+st.image("https://imortaisdofutebol.com/jogos-eternos-flamengo-3x0-liverpool-1981/)
+         
 st.write("2013: Felipe, Léo Moura, Samir, Chicão, André Santos, Amaral, Luiz Antônio, Elias, Carlos Eduardo, Hernane, Paulinho, Jayme de Almeida")
 st.write("2019: Diego Alves, Rafinha, Rodrigo Caio, Pablo Marí, Filipe Luís, Willian Arão, Gerson, Giorgian de Arrascaeta, Éverton Ribeiro, Bruno Henrique, Gabriel B., Jorge Jesus")
 st.write("2020: Diego Alves, Isla, Rodrigo Caio, Willian Arão, Filipe Luís, Diego Ribas, Gerson, Giorgian de Arrascaeta, Éverton Ribeiro, Bruno Henrique, Gabriel B., Rogério Ceni")
