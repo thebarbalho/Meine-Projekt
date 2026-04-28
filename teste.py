53aec376a96252fd0f8a5a948d8c5d5bd7d1084f
thebarbalho <victorbarbalhomartins@gmail.com>
2026-04-28 15:18:20 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,13 +1,17 @@
 import streamlit as st
 
 st.title('FLAMENGO - CAMPEÕES')
-
-st.write("1981: Raul, Leandro, Marinho, Mozer, Junior, Adílio, Andrade, Zico, Tita, Nunes, Lico, Carpegiani")
 st.image("https://imortaisdofutebol.com/jogos-eternos-flamengo-3x0-liverpool-1981/")
-         
+st.write("1981: Raul, Leandro, Marinho, Mozer, Junior, Adílio, Andrade, Zico, Tita, Nunes, Lico, Carpegiani")         
+
 st.write("2013: Felipe, Léo Moura, Samir, Chicão, André Santos, Amaral, Luiz Antônio, Elias, Carlos Eduardo, Hernane, Paulinho, Jayme de Almeida")
+
 st.write("2019: Diego Alves, Rafinha, Rodrigo Caio, Pablo Marí, Filipe Luís, Willian Arão, Gerson, Giorgian de Arrascaeta, Éverton Ribeiro, Bruno Henrique, Gabriel B., Jorge Jesus")
+
 st.write("2020: Diego Alves, Isla, Rodrigo Caio, Willian Arão, Filipe Luís, Diego Ribas, Gerson, Giorgian de Arrascaeta, Éverton Ribeiro, Bruno Henrique, Gabriel B., Rogério Ceni")
+
 st.write("2022: Santos, Rodinei, David Luiz, Léo Pereira, Filipe Luís, João Gomes, Thiago Maia, Giorgian de Arrascaeta, Éverton Ribeiro, Gabriel B., Pedro, Dorival Jr.")
+
 st.write("2024: Rossi, Wesley, Léo Ortiz, Léo Pereira, Alex Sandro, Pulgar, Gerson, Giorgian de Arrascaeta, Gonzalo Plata, Gabriel B., Bruno Henrique, Filipe Luís")
+
 st.write("2025: Rossi, Varela, Danilo, Léo Pereira, Alex Sandro, Pulgar, Jorginho, Giorgian de Arrascaeta, Samuel Lino, Bruno Henrique, Pedro, Filipe Luís")
