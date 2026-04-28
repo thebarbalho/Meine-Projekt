f643b7ebc7ac35d5c4de35a15955e6a90bf16db0
thebarbalho <victorbarbalhomartins@gmail.com>
2026-04-28 15:13:32 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -6,4 +6,5 @@ st.write("2013: Felipe, Léo Moura, Samir, Chicão, André Santos, Amaral, Luiz 
 st.write("2019: Diego Alves, Rafinha, Rodrigo Caio, Pablo Marí, Filipe Luís, Willian Arão, Gerson, Giorgian de Arrascaeta, Éverton Ribeiro, Bruno Henrique, Gabriel B., Jorge Jesus")
 st.write("2020: Diego Alves, Isla, Rodrigo Caio, Willian Arão, Filipe Luís, Diego Ribas, Gerson, Giorgian de Arrascaeta, Éverton Ribeiro, Bruno Henrique, Gabriel B., Rogério Ceni")
 st.write("2022: Santos, Rodinei, David Luiz, Léo Pereira, Filipe Luís, João Gomes, Thiago Maia, Giorgian de Arrascaeta, Éverton Ribeiro, Gabriel B., Pedro, Dorival Jr.")
+st.write("2024: Rossi, Wesley, Léo Ortiz, Léo Pereira, Alex Sandro, Pulgar, Gerson, Giorgian de Arrascaeta, Gonzalo Plata, Gabriel B., Bruno Henrique, Filipe Luís")
 st.write("2025: Rossi, Varela, Danilo, Léo Pereira, Alex Sandro, Pulgar, Jorginho, Giorgian de Arrascaeta, Samuel Lino, Bruno Henrique, Pedro, Filipe Luís")
