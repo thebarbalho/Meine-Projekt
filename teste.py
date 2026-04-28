366a3045fa2b1b44c6e72949f003e38919adeb58
thebarbalho <victorbarbalhomartins@gmail.com>
2026-04-28 15:05:15 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title('FLAMENGO 1981 - 2019 - 2022 - 2025')
 st.write("1981: Raul, Leandro, Marinho, Mozer, Junior, Adílio, Andrade, Zico, Tita, Nunes, Lico, Carpegiani")
-st.write("2019: Diego Alves, Rafinha, Rodrigo Caio, Pablo Marí, Filipe Luís, Willian Arão, Gerson, Giorgian de Arrascaeta, Éverton Ribeiro, Bruno Henrique, Gabriel B., Jorge Jesus)
-st.write("2022: Santos, Rodinei, David Luiz, Léo Pereira, Filipe Luís, João Gomes, Thiago Maia, Giorgian de Arrascaeta, Éverton Ribeiro, Gabriel B., Pedro, Dorival Jr.)
-st.write("2025: Rossi, Varela, Danilo, Léo Pereira, Alex Sandro, Pulgar, Jorginho, Giorgian de Arrascaeta, Samuel Lino, Bruno Henrique, Pedro, Filipe Luís)
+st.write("2019: Diego Alves, Rafinha, Rodrigo Caio, Pablo Marí, Filipe Luís, Willian Arão, Gerson, Giorgian de Arrascaeta, Éverton Ribeiro, Bruno Henrique, Gabriel B., Jorge Jesus")
+st.write("2022: Santos, Rodinei, David Luiz, Léo Pereira, Filipe Luís, João Gomes, Thiago Maia, Giorgian de Arrascaeta, Éverton Ribeiro, Gabriel B., Pedro, Dorival Jr.")
+st.write("2025: Rossi, Varela, Danilo, Léo Pereira, Alex Sandro, Pulgar, Jorginho, Giorgian de Arrascaeta, Samuel Lino, Bruno Henrique, Pedro, Filipe Luís")
