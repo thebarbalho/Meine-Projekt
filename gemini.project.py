7d6102b8de2605ff190e7239c1d6308bfa329c23
thebarbalho <victorbarbalhomartins@gmail.com>
2026-05-15 11:13:07 -0300

    Update gemini.project.py

--- gemini.project.py
+++ gemini.project.py
@@ -1,10 +1,8 @@
 import streamlit as st
 from google import genai
 
-# SENHAS DE ACESSO AO APP
-
 GEMINI_API_KEY= 'AIzaSyBJRSrbzvLVO7NIs7irPm1VCCebE6Lq9zE'
-APP_PASSWORD=2908
+APP_PASSWORD= "2908"
 
 if "autenticado" not in st.session_state:
     st.session_state.autenticado = False
@@ -14,8 +12,13 @@ def login():
 
     senha = st.text_input("Digite a senha:", type="password")
 
-if st.button("Entrar"):
-    if senha == APP_PASSWORD:
-        st.session_state.autenticado = True
-    else:
-        st.error("Senha incorreta!")
+    if st.button("Entrar"):
+        if senha == APP_PASSWORD:
+            st.session_state.autenticado = True
+        else:
+            st.error("Senha incorreta!")
+
+if not st.session_state.autenticado:
+    login()
+else:
+    st.write("Você está logado!")
