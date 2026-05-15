4b997f967fe5147d5f0485eda2c1989297106fb0
thebarbalho <victorbarbalhomartins@gmail.com>
2026-05-15 10:59:20 -0300

    Update gemini.project.py

--- gemini.project.py
+++ gemini.project.py
@@ -1,18 +1,26 @@
 import streamlit as st
-
-os.secrets["GOOGLE_API_KEY"] = userdata.get('GEMINI_API_KEY')
-
-# Configura o cliente da SDK do Gemini
-
 import google.generativeai as genai
 
-MODEL_ID = "gemini-2.5-flash"
+/// # SENHAS DE ACESSO AO APP ///
 
-for m in genai.list_models():
-  if 'generateContent' in m.supported_generation_methods:
-    print(m.name)
+GEMINI_API_KEY= AIzaSyBJRSrbzvLVO7NIs7irPm1VCCebE6Lq9zE
+APP_PASSWORD=2908
 
-# Pergunta ao Gemini uma informação mais recente que seu conhecimento
+if "autenticado" not in st.session_state:
+    st.session_state.autenticado = False
+
+def login():
+    st.title("LOGIN NO APP")
+
+    senha = st.text_input("Digite a senha:", type="password")
+
+    if st.button("Entrar"):
+        if senha == APP_PASSWORD:
+            st.session_state.autenticado = True
+        else:
+            st.error("Senha incorreta!")
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 from IPython.display import HTML, Markdown
 
