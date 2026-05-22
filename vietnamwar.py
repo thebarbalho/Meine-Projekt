08b408363739f687e87eaa85f14bafa1bbd145ec
thebarbalho <victorbarbalhomartins@gmail.com>
2026-05-22 10:47:47 -0300

    Update and rename coldwar.py to vietnamwar.py

---
+++ vietnamwar.py
@@ -0,0 +1,13 @@
+# REQUISIÇÕES
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# INTERFACE NO STREAMLIT
+st.title('GRÁFICOS SOBRE A GUERRA DO VIETNÃ')
+st.image('https://thumbs.dreamstime.com/b/vietnam-e-estados-unidos-bandeira-dos-eua-do-vietname-numa-parede-rachada-da-am%C3%A9rica-rela%C3%A7%C3%B5es-com-o-269650766.jpg')
+# GRÁFICO SOBRE AS MORTES DURANTE A GUERRA DO VIETNÃ
+
+# GRÁFICO SOBRE AS TROCAS DE CHEFES DE ESTADO DURANTE A GUERRA DO VIETNÃ
+
+# GRÁFICO TEMPORAL DE CHEFES DE ESTADO DURANTE A GUERRA DO VIETNÃ
