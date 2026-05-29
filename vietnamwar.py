fe7349f52be3c792e594dd33a09604edc736e3e0
thebarbalho <victorbarbalhomartins@gmail.com>
2026-05-28 23:31:06 -0300

    Update vietnamwar.py

--- vietnamwar.py
+++ vietnamwar.py
@@ -2,12 +2,10 @@
 import streamlit as st
 import pandas as pd
 
+
 # INTERFACE NO STREAMLIT
-st.title('GRÁFICOS SOBRE A GUERRA DO VIETNÃ (1955-1975)')
+st.title('GRÁFICOS SOBRE OS ESTADOS UNIDOS DURANTE A GUERRA DO VIETNÃ (1955-1975)')
 st.image('https://thumbs.dreamstime.com/b/vietnam-e-estados-unidos-bandeira-dos-eua-do-vietname-numa-parede-rachada-da-am%C3%A9rica-rela%C3%A7%C3%B5es-com-o-269650766.jpg')
 
 # GRÁFICO SOBRE AS MORTES DURANTE A GUERRA DO VIETNÃ
 
-# GRÁFICO SOBRE AS TROCAS DE CHEFES DE ESTADO DURANTE A GUERRA DO VIETNÃ
-
-# GRÁFICO TEMPORAL DE CHEFES DE ESTADO DURANTE A GUERRA DO VIETNÃ
